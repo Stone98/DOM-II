61d82ef9d377f03c345841b18a4f4446e3049cda
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-10-27 18:58:20 -0700

    complete

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ const introHeader = document.querySelector('.intro');
 const introImage = introHeader.querySelector('img');
 const home = document.querySelector('#homeLink');
 const bodyPage = document.querySelector('body');
-const imageContent = document.getElementsByClassName('img-content');
-
+const imageContent = document.getElementsByClassName('img-content')[0];
+const contentDest = document.getElementsByClassName('content-destination')[0];
+const imageContnet = contentDest.querySelector('img');
 // mouseover event
 navLinks.addEventListener('mouseover', (event) => {
     event.target.style.color = 'green';
@@ -57,15 +58,50 @@ window.addEventListener('resize', (event) => {
 })
 
 // scroll event
-// bodyPage.addEventListener('scroll', (event) => {
+window.addEventListener('scroll', function() {
+    let elm = document.getElementById('scroll');
+    if (elm == null) {
+        elm = document.createElement('span');
+        elm.setAttribute("id", "scroll");
+        elm.style.color = 'blue';
+        elm.style.fontSize = '2em';
+        elm.style.position = 'absolute';
+        elm.style.left = '10px';
+        elm.style.top = '200px';
+        document.body.prepend(elm);
 
-// })
+    }
+    elm.innerHTML = window.pageYOffset + 'px';
+});
 
 // select event
-
+// no <input type="text"> or <textarea>
 // dblclick event
 imageContent.addEventListener('dblclick', (event) => {
-    event.target.style.transform = '1.3em';
+    event.target.style.opacity = .5;
 })
 
 // drag / drop event
+
+// contentDest.addEventListener('dragover', (event) => {
+//     console.log('whatever');
+// })
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    imageContnet.src = ev.srcElement.querySelector("img").src;
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+navLinks.addEventListener('click', (event) => {
+    event.preventDefault();
+});
